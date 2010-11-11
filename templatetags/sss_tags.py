6f9c961e410b37766d65e2465e925b3ee8e6f65f
Bruno Bord <bruno@jehaisleprintemps.net>
2010-11-11 01:09:39 +0100

    project summary in change list
    --HG--
    branch : admin-templates

---
+++ templatetags/sss_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from django.db.models import Sum
+from django.utils.translation import ugettext as _
+from sss.models import BacklogItem
+
+
+register = template.Library()
+
+@register.simple_tag
+def project_summary():
+    qs = BacklogItem.objects.filter(done=False)
+    if qs.count():
+        return " / ".join((
+            _("Tasks remaining %d") % qs.count(),
+            _("Remaining points %(story_points__sum)d") % qs.aggregate(Sum('story_points'))
+        ))
+    else:
+        return ""

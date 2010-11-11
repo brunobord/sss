f34f8aee308750c147d2bc5b5c73acd35e50384b
Bruno Bord <bruno@jehaisleprintemps.net>
2010-11-11 01:32:46 +0100

    mark as done action for selected tasks

--- admin.py
+++ admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 from sss.models import BacklogItem
+
+# Action(s)
+def mark_as_done(modeladmin, request, queryset):
+    queryset.update(done=True)
+mark_as_done.short_description = _("Mark these items as done")
 
 class BacklogItemAdmin(admin.ModelAdmin):
     ordering = ('-priority',)
     list_display = ['label', 'priority', 'story_points', 'done']
     list_filter = ('done',)
     readonly_fields = ('date_created', 'date_modified')
-    
+    actions = [mark_as_done]
 
 admin.site.register(BacklogItem, BacklogItemAdmin)

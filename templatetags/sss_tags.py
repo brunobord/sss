aa6724a251d146bc58d3a7e6bef806c049b74f95
Bruno Bord <bruno@jehaisleprintemps.net>
2010-11-19 21:23:51 +0100

    burndown chart assez simple, mais qui marche pas mal

--- templatetags/sss_tags.py
+++ templatetags/sss_tags.py
@@ -1,3 +1,4 @@
+import datetime
 from django import template
 from django.db.models import Sum
 from django.utils.translation import ugettext as _
@@ -16,3 +17,26 @@ def project_summary():
         ))
     else:
         return _("Either your project hasn't started, or it's completely done! Next?")
+
+
+@register.simple_tag
+def project_burndown_url():
+    qs = BacklogItem.objects.all()
+    first = qs.order_by('date_created')[0]
+
+    total_points = qs.aggregate(Sum('story_points'))['story_points__sum']
+
+    ideal_data = []
+    current_data = []
+    for x in range(0, 14):
+        points = total_points - (total_points * (x/14.0))
+        ideal_data.append(str(points))
+        # current
+        current_day = first.date_created + datetime.timedelta(days=x)
+        points = qs.filter(done=True, date_modified__lte=current_day).aggregate(Sum('story_points'))['story_points__sum']
+        if points is None:
+            points = 0
+        current_data.append(str(total_points - points))
+    ideal_data.append('0')
+
+    return "http://chart.apis.google.com/chart?chs=600x250&chtt=Burndown&cht=lc&chdl=estimated|actual&chco=FF0000,00FF00&chxr=0,0,30,2|1,0,40,2&chds=0,40&chd=t:%(ideal_data)s|%(current_data)s" % {'ideal_data': ",".join(ideal_data), 'current_data': ",".join(current_data)}

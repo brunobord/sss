f1aa39122a793a21b336f89e82982a16150076fa
Bruno Bord <bruno@jehaisleprintemps.net>
2010-11-21 17:52:19 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "sss",
-    version = "1.0",
+    version = "1.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=['Django>=1.2', 'distribute>=0.6'],
